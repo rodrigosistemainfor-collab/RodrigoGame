d02c19da2ad90a6c58f8d009e8ff713764a69f1c
RodrigoM <rodrigo.sistemainfo@gmail.com>
2026-03-07 17:38:53 -0300

    Creating asset folder Inserting all assets Creating the menu: backgrung, sound and fonts Creating the const file

---
+++ Const.py
@@ -0,0 +1,16 @@
+#C
+COLOR_ORANGE = (255,128,0)
+COLOR_WHITE = (255,255,255)
+COLOR_BLACK = (0,0,0)
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+                'NEW GAME 2P - COOPERATIVE',
+                'NEW GAME 2P - COMPETITIVE',
+                'SCORE',
+                'EXIT')
+
+#W
+WIN_WIDTH = 840
+WIN_HEIGHT = 560
+
--- code/menu.py
+++ code/menu.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_BLACK
+
 
 class Menu:
-    def __init__(self,window):
+    def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load("./asset/MenuBg.png.png")
+        self.rect = self.surf.get_rect(left=0, top=0)
 
-    def run(self, ):
-        pass
+    def run(self, MENU=None):
+        pygame.mixer_music.load('./asset/Menu.mp3.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(text_size=50, text="RODRIGO", text_color=COLOR_ORANGE, text_center_pos=((WIN_WIDTH / 2), 70))
+            self.menu_text(text_size=50, text="GAME", text_color=COLOR_ORANGE, text_center_pos=((WIN_WIDTH / 2), 150))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(text_size=40, text=MENU_OPTION[i], text_color=COLOR_BLACK,
+                               text_center_pos=((WIN_WIDTH / 2), 300 + 50 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close Window
+                    quit()  # End pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
