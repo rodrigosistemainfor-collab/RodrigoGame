12296aea835b0e2e52cfb327a453746d6925523d
RodrigoM <rodrigo.sistemainfo@gmail.com>
2026-03-11 15:19:48 -0300

    Creating Mediator Pattern. Enemies destruction.

--- Const.py
+++ Const.py
@@ -7,6 +7,22 @@ COLOR_BLACK = (0, 0, 0)
 COLOR_YELLOW = (255, 253, 85)
 
 # E
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Player1': 300,
+    #   'Player1Shot': 1,
+    'Player2': 300,
+    #   'Player2Shot': 1,
+    'Enemy1': 50,
+    #   'Emeny1Shot': 1,
+    'Enemy2': 60,
+    #    'Emeny2Shot': 1,
+
+}
+
 EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0': 0,
@@ -16,9 +32,8 @@ ENTITY_SPEED = {
     'Player1': 3,
     'Player2': 3,
     'Enemy1': 2,
-    'Enemy2':1,
+    'Enemy2': 1,
 }
-
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
--- code/level.py
+++ code/level.py
@@ -8,6 +8,7 @@ from pygame import Surface, Rect
 from pygame.font import Font
 
 from Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
+from code.EntityMediator import EntityMediator
 from code.entity import Entity
 from code.entityFactory import EntityFactory
 
@@ -65,6 +66,9 @@ class Level:
                 text_pos=(10, WIN_HEIGHT - 20)
             )
             pygame.display.flip()
+            # collisions
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
 
         pass  # até aqui ok
 
